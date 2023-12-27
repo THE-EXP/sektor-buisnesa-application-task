ebb8126397485080234a2ddc57c7938e38fa8a07
THE_EXP <105677206+THE-EXP@users.noreply.github.com>
2023-12-27 20:38:44 +0300

    add modules, start with sequelize

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const sql = require('sequelize');
+const path = require('path');
+const fs = require('fs');
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
 require('dotenv').config('./');
 const app = express();
 
@@ -9,7 +13,16 @@ app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-const db = new sql('users', 'root', process.env.DBPWD, {
+//logic below checks for .env file, if .env file has db credentials, and if it has a 'secret' value assigned, and if not, generates one
+if (!fs.existsSync('.env')) {
+  throw new Error('No environment variables loaded, please create a .env file in the root folder(i.e. in the same folder as thу server)');
+}
+if (!process.env.DBPWD || !process.env.DBUSER) {
+  throw new Error('No database credentials stored, please add them to the .env file (DBUSER and DBPWD keys, plaintext)');
+}
+const secret = process.env.SECRET || crypto.randomBytes(32).toString('hex'); //required for jwt token validation
+
+const db = new sql('users', process.env.DBUSER, process.env.DBPWD, {
     host: 'localhost',
     dialect: 'mysql',
 });
@@ -20,24 +33,42 @@ db.authenticate().then(() => {
     console.error(`Unable to connect to the database, exited with:\n${error}`);
 });
 
+const Model = db.define('user', {
+    id: {
+        type: sql.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    firstname: {type: sql.STRING, allowNull: false},
+    email: {type: sql.STRING, allowNull: false},
+    surname: {type: sql.STRING, allowNull: false},
+    pwdhash: {type: sql.STRING, allowNull: false},
+    pwdsalt: {type: sql.STRING, allowNull: false},
+    gender: {type: sql.BOOLEAN, allowNull: false}, //* True = Male, False = Female, a bit more efficient db usage
+    registration_date: {type: sql.DATE, allowNull: false, defaultValue: sql.NOW}
+}, {
+  tableName: 'users'
+});
+Model.sync({alter: true});
+
 app.post('/user/login', (req, res) => {
-  // Handle post request for route1
+  login(req, res)// Login logic(JWT)
 });
 
 app.post('/user/register', (req, res) => {
-  // Handle post request for route2
+  register(req, res); // Registration logic (jwt)
 });
 
 app.put('/profile/:id', (req, res) => {
-  // Handle put request for route3
+  editUser(req, res)// Edit logic
 });
 
 app.get('/profile/:id', (req, res) => {
-  // Handle get request for route4
+  showOne(req, res)// showOne logic
 });
 
 app.get('/profiles', (req, res) => {
-  // Handle get request for route5
+  showAll(req, res)// showAll logic
 });
 
 app.listen(process.env.MAIN_PORT || 8080, () => {
