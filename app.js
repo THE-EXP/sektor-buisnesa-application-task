69d140c6916f4e71f020df30e2317c71f38607f5
THE_EXP <105677206+THE-EXP@users.noreply.github.com>
2023-12-31 19:30:29 +0300

    Multiple implementations
    Rename sql to be sequelize;
    implement rudimentary login & register functions;
    implement showAll logic, ordered by registration_date in either order
    
    //Happy New Year!

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const sql = require('sequelize');
+const sequelize = require('sequelize');
 const path = require('path');
 const fs = require('fs');
 const crypto = require('crypto');
 const bcrypt = require('bcrypt');
 const cp = require('cookie-parser');
+const multer = require('multer');
+const upload = multer({dest: './pictures/'});
+const paginate = require('paginate-array');
 require('dotenv').config('./');
 const app = express();
 
-app.use(express.json());
+//configure multer to upload to ./pictures/ and check if the file is actually an image
+app.use(cp());
+app.use(upload.single('file'));
 app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +28,7 @@ if (!process.env.DBPWD || !process.env.DBUSER) {
 }
 const secret = process.env.SECRET || crypto.randomBytes(32).toString('hex'); //required for jwt token validation
 
-const mysql = new sql('users', process.env.DBUSER, process.env.DBPWD, {
+const mysql = new sequelize('users', process.env.DBUSER, process.env.DBPWD, {
     host: 'localhost',
     dialect: 'mysql',
 });
@@ -31,49 +36,47 @@ const mysql = new sql('users', process.env.DBUSER, process.env.DBPWD, {
 mysql.authenticate().then(() => {
     console.log(`Connection has been established successfully`);
 }).catch((error) => {
-    console.error(`Unable to connect to the database, exited with:\n${error}`);
+    console.error(`Unable to connect to the database, exited with:`);
+    throw error;
 });
 
 const Model = mysql.define('user', {
     id: {
-        type: sql.INTEGER,
+        type: sequelize.INTEGER,
         primaryKey: true,
         autoIncrement: true
     },
-    firstname: {type: sql.STRING, allowNull: false},
-    email: {type: sql.STRING, allowNull: false},
-    surname: {type: sql.STRING, allowNull: false},
-    pwdhash: {type: sql.STRING, allowNull: false},
-    pwdsalt: {type: sql.STRING, allowNull: false},
-    gender: {type: sql.CHAR, allowNull: false}, //* M = Male, F = Female, U = Unspecified/Unknown
-    registration_date: {type: sql.DATE, allowNull: false, defaultValue: sql.NOW},
-    profile_picture: {type: sql.STRING, allowNull: false, defaultValue: 'default.png'},
+    firstname: {type: sequelize.STRING, allowNull: false},
+    email: {type: sequelize.STRING, allowNull: false},
+    surname: {type: sequelize.STRING, allowNull: true},
+    pwdhash: {type: sequelize.STRING, allowNull: false},
+    pwdsalt: {type: sequelize.STRING, allowNull: false},
+    gender: {type: sequelize.CHAR, allowNull: false, defaultValue: 'U'}, //* M = Male, F = Female, U = Unspecified
+    registration_date: {type: sequelize.DATE, allowNull: false, defaultValue: sequelize.NOW},
+    profile_picture: {type: sequelize.STRING, allowNull: false, defaultValue: './pictures/default.png'},
 }, {
   tableName: 'users'
 });
 Model.sync({alter: true});
 
-//generate json web token
-
-
 app.post('/user/login', (req, res) => {
-  login(req, res)// Login logic(JWT?)
+  login(req, res); // Login logic(JWT? How to maintain?!) 
 });
 
 app.post('/user/register', (req, res) => {
-  register(req, res); // Registration logic (JWT?)
+  register(req, res); // Registration logic (JWT? how to maintain?!)
 });
 
 app.put('/profile/:id', (req, res) => {
-  editUser(req, res)// Edit logic(require JWT to confirm user?)
+  editUser(req, res); // Edit logic(require JWT to confirm that an actual owner of account is editing it? How to maintain?!)
 });
 
 app.get('/profile/:id', (req, res) => {
-  showOne(req, res)// showOne logic
+  showOne(req, res); // showOne logic, JSON response
 });
 
 app.get('/profiles', (req, res) => {
-  showAll(req, res)// showAll logic
+  showAll(req, res); // showAll logic, how to make pagination work in this? use paginate-array??, JSON response
 });
 
 app.listen(process.env.MAIN_PORT || 8080, () => {
@@ -81,17 +84,47 @@ app.listen(process.env.MAIN_PORT || 8080, () => {
 })
 
 async function register(req, res) {
-  const { firstname, email, password } = req.body;
+  //TODO: add JWT to this crap
+  const firstname = req.body.firstname;
+  const email = req.body.email;
+  const password = req.body.password; 
 
   if (!firstname || !email || !password) {
+    console.log(firstname, email, password);
     res.status(400).send('Fill in all the missing fields');
-  } else {
-    const pwdsalt = await bcrypt.genSalt();
-    const pwdhash = await bcrypt.hash(password, pwdsalt);
-    const user = await Model.create({ firstname, email, pwdhash, pwdsalt });
-    user.save();
-    res.status(201).json({ result: 'OK', msg: 'User successfully registered!' });
+    return;
+  }
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(password, salt);
+    const user = await Model.create({ firstname, email, pwdhash: hash, pwdsalt: salt });
+    await user.save();
+    res.status(201).cookie('hashpwd', hash, {maxAge: 86400, httpOnly: true, secure: true}).json({ result: 'OK', msg: 'User successfully registered!' });
+}
+
+async function login(req, res){
+  // TODO: add JWT to this crap, for now just check password
+  const email = req.body.email;
+  const password = req.body.password;
+  const user = await Model.findOne({where: {email: email}});
+  console.log(user);
+  //const hashedpwd = req.cookies.hashpwd;
+  switch (await bcrypt.compare(password, user.pwdhash)) {
+    case true:
+      console.log(`User ${user.id} logged in`);
+      break;
+    case false:
+      console.log(`User ${user.id} failed to log in, wrong password`);
   }
 }
 
-//asynchronous function to generate jwt access/refresh token pair using RS256
+async function showAll(req, res) {
+  if (req.query.page == undefined) {
+    req.query.page = 1; //assume page 1 by default
+  }
+  if (req.query.orderby == undefined) {
+    req.query.orderby = 'ASC'; //assume ascending order by default(oldest first)
+  }
+  const offset = (req.query.page - 1) * 10;
+  const users = await mysql.query(`SELECT * FROM users.users ORDER BY registration_date ${req.query.orderby.toUpperCase()} LIMIT 10 OFFSET ${offset}`, {type: sequelize.QueryTypes.SELECT});
+  res.status(200).json({ ok: true, result: users});
+}
